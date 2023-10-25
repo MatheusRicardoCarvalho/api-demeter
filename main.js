a344c35075b6d81a2b972768cfb58cd101838817
Matheus Ricardo Carvalho Silva <49125354+MatheusKanmei@users.noreply.github.com>
2023-10-25 15:15:35 -0300

    Update main.js
    porta padrão mudada pra 80

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ app.post('/sensor/temp', (req, res) => {
 
 
 //Inícia o serviço
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT}!`);
